ac7307c0c603c5135c47e40fb988375ae43d3e75
DarrenVictoria <darrenvictoria2@gmail.com>
2023-06-09 16:01:45 +0530

    Kilometers to Meters C# Code

---
+++ Kilometer to Meters/Kilometer to Meters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kilometer_to_Meters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues obj = new ConvertValues();
+            obj.kilometerTOmeter();
+            Console.ReadKey();
+        }
+    }
+}
